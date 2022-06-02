d0428030c84c51f6b1ca28f08829811effa88ee5
Skyere <70713060+Skyere@users.noreply.github.com>
2022-06-02 19:10:00 +0300

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,12 +2,12 @@ export const environment = {
   production: true,
   apiKeys: 'AIzaSyDanBliCzAuCZrsq67FeKEs3vqAilUD_is',
   apiMapKey: 'AIzaSyB3xs7Kczo46LFcQRFKPMdrE0lU4qsR_S4',
-  backendLink: 'https://greencity-prod.azurewebsites.net/',
+  backendLink: 'http://greencity.5dc2a41bb1cb4f3e9211.uksouth.aksapp.io/',
   backendChatLink: 'https://greencity-chat-prod.azurewebsites.net/',
-  backendUserLink: 'https://greencity-user-prod.azurewebsites.net/',
-  backendUbsLink: 'https://greencity-ubs-prod.azurewebsites.net',
-  frontendLink: 'https://greencity-front-prod.azurewebsites.net/GreenCityClient/',
-  socket: 'https://greencity-prod.azurewebsites.net/socket',
+  backendUserLink: 'http://greencity-user.5dc2a41bb1cb4f3e9211.uksouth.aksapp.io/',
+  backendUbsLink: 'http://greencity-ubs.5dc2a41bb1cb4f3e9211.uksouth.aksapp.io',
+  frontendLink: 'http://greencity-front.5dc2a41bb1cb4f3e9211.uksouth.aksapp.io/GreenCityClient/',
+  socket: 'http://greencity.5dc2a41bb1cb4f3e9211.uksouth.aksapp.io/socket',
   chatSocket: 'https://greencity-chat-prod.azurewebsites.net/socket',
   firebaseConfig: {
     apiKey: 'AIzaSyDSVxahTHqdffRX2upKIMXCCjEBUYoHX8E',
@@ -20,8 +20,8 @@ export const environment = {
     measurementId: 'G-CCHRKQ4R0S'
   },
   ubsAdmin: {
-    backendEmployeesLink: 'https://greencity-ubs-prod.azurewebsites.net/admin/ubs-employee/getAll-active-employees',
-    backendUbsAdminLink: 'https://greencity-ubs-prod.azurewebsites.net/ubs'
+    backendEmployeesLink: 'http://greencity-ubs.5dc2a41bb1cb4f3e9211.uksouth.aksapp.io/admin/ubs-employee/getAll-active-employees',
+    backendUbsAdminLink: 'http://greencity-ubs.5dc2a41bb1cb4f3e9211.uksouth.aksapp.io/ubs'
   },
   googleClientId: '129513550972-eu9ej46rviv1ac8q14at62t2k5qon1pu.apps.googleusercontent.com',
   agmCoreModuleApiKey: 'AIzaSyCdBtR8O8eazfWUSdyb5O2cnL32uJtWUwA'
